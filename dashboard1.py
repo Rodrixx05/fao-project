4c142752f791638d11a4f629fd6bc2d3cc76bb94
= <marc.rodri5#@gmail.com>
2022-02-06 18:47:35 +0100

    Dashboard update and presentable

--- dashboard1.py
+++ dashboard1.py
@@ -20,7 +20,7 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.CERULEAN])
 app.layout = html.Div(
     [
     dbc.Row(dbc.Col(html.H1('Food distribution and environmental impact overview', style={'textAlign': 'center'}))),
-    dbc.Row(dbc.Col(dcc.Dropdown(id = 'food_dropdown', options = items_list, placeholder = 'Select a food', value = 'Lemons and limes'), width = 6), justify='center', align='center'),
+    dbc.Row(dbc.Col(dcc.Dropdown(id = 'food_dropdown', options = items_list, placeholder = 'Select a food', value = 'Meat, chicken'), width = 6), justify='center', align='center'),
     dbc.Row(
         [dbc.Col(dcc.Graph(id = 'graph_map', figure = {}), width = 8),
         dbc.Col(html.Div(id='table_map'), width = 4)], align = 'center', style = {'margin-right': 5}),
@@ -32,9 +32,16 @@ app.layout = html.Div(
     dbc.Row(
         [dbc.Col(dcc.RadioItems(id = 'population_graph', options = ['Totals', 'Per Capita'], value = 'Totals', inputStyle={"margin-left": "20px", "margin-right": "5px"}), width = {'size': 3, 'offset': 1}, style = {'margin-left': 5}),
         dbc.Col(dcc.Dropdown(id = 'country_dropdown', options = df['Area'].unique(), multi = True, placeholder = 'Select a country', value = ['Spain', 'Italy', 'Netherlands']), width = {'size': 5, 'offset': 0}),
-        dbc.Col(dcc.Slider(df['Year'].min(), df['Year'].max(), step = None, value = df['Year'].min(), marks={str(year): str(year) for year in df['Year'].unique()}, id='year_slider_comparison'), width = {'size': 3, 'offset': 0}, style = {'margin-right': 5})], justify='center')
+        dbc.Col(dcc.Slider(df['Year'].min(), df['Year'].max(), step = None, value = df['Year'].min(), marks={str(year): str(year) for year in df['Year'].unique()}, id='year_slider_comparison'), width = {'size': 3, 'offset': 0}, style = {'margin-right': 5, 'margin-bottom': 30})], justify='center'),
+    dbc.Row(
+        [dbc.Col(dcc.Graph(id = 'graph_reg', figure = {}), width = 7, style = {'margin-left': 5}),
+        dbc.Col([dbc.Row(
+            html.Div(id='table_reg_most')
+        ), dbc.Row(
+            html.Div(id='table_reg_least')
+        )], width = 4, align = 'center', style = {'margin-right': 5})]),
+    dbc.Row(dbc.Col(dcc.Slider(df['Year'].min(), df['Year'].max(), step = None, value = df['Year'].min(), marks={str(year): str(year) for year in df['Year'].unique()}, id='year_slider_reg'), width = {'size': 6, 'offset': 3}))
     ]
-
 )
 
 @app.callback(
@@ -80,7 +87,7 @@ def update_map(food_dropdown, year_slider_map, valor, population_map):
 
     return_df = graph_df[['Area', 'Value']].sort_values(['Value'], ascending = False).head(5)
     return_df['Value'] = return_df['Value'].map('{:.2f}'.format)
-    labels['Area'] = 'Country'
+    labels['Area'] = 'Top Countries'
     return_df.rename(labels, axis = 1, inplace = True)
 
     return fig, dbc.Table.from_dataframe(return_df, striped=True, bordered=True)
@@ -118,6 +125,51 @@ def update_comparison(food_dropdown, country_dropdown, year_slider_comparison, p
     title = f"{food_dropdown} emissions' origin comparator between countries during {year_slider_comparison}"
     fig = px.bar(graph_df, x="Area", y="Value", color="Element", barmode = 'group', range_y = [0, graph_df['Value'].max() * 1.2], labels = labels, title = title, hover_data=['Partner Countries'])
     return fig
+
+@app.callback(
+    [Output('graph_reg', 'figure'),
+        Output('table_reg_most', 'children'),
+        Output('table_reg_least', 'children')],
+    Input('food_dropdown', 'value'),
+    Input('year_slider_reg', 'value'),
+)
+
+def update_reg(food_dropdown, year_slider_reg):
+    elements = ['Emissions (CO2eq)', 'Production']
+    graph_df = df[(df['Element'].isin(elements)) & (df['Item'] == food_dropdown) & (df['Area'] == df['Partner Countries']) & (df['Year'] == year_slider_reg)]
+    graph_df.drop(graph_df[graph_df['Value'] == 0].index, inplace = True)
+
+    graph_df.loc[:, 'Value'] = graph_df.apply(lambda x: x['Value'] * 1000 if x['Element'] == 'Emissions (CO2eq)' else x['Value'], axis = 1).copy()
+    graph_df.loc[:, 'Unit'] = graph_df['Unit'].apply(lambda x: 'tonnes CO2' if x == 'gigagrams CO2' else x).copy()
+
+    graph_df = graph_df.pivot_table(values = 'Value', index = 'Area', columns = ['Element'])
+    graph_df.columns = ['CO2 Emissions [tonnes]', 'Production [tonnes]']
+    graph_df['Emissions Intensity'] = graph_df['CO2 Emissions [tonnes]'] / graph_df['Production [tonnes]']
+    title = f'CO2 emissions derived from {food_dropdown.lower()} production during {year_slider_reg}'
+
+    fig = px.scatter(
+        data_frame = graph_df, 
+        x = 'Production [tonnes]', 
+        y = 'CO2 Emissions [tonnes]', 
+        trendline = 'ols', 
+        trendline_color_override = '#fd8585',
+        trendline_scope = 'overall', 
+        hover_name = graph_df.index,
+        hover_data = ['Emissions Intensity'],
+        color = graph_df.index, 
+        title = title)
+    
+    fig.update_layout(showlegend = False)
+
+    return_df_most = graph_df.reset_index()[['Area', 'Emissions Intensity']].sort_values(['Emissions Intensity'], ascending = False).head(3)
+    return_df_most['Emissions Intensity'] = return_df_most['Emissions Intensity'].map('{:.3f}'.format)
+    return_df_most.rename({'Area': 'Top Countries', 'Emissions Intensity': 'TCO2/Tfood'}, axis = 1, inplace = True)
+
+    return_df_least = graph_df.reset_index()[['Area', 'Emissions Intensity']].sort_values(['Emissions Intensity']).head(3)
+    return_df_least['Emissions Intensity'] = return_df_least['Emissions Intensity'].map('{:.3f}'.format)
+    return_df_least.rename({'Area': 'Least Countries', 'Emissions Intensity': 'TCO2/Tfood'}, axis = 1, inplace = True)
+    
+    return fig, dbc.Table.from_dataframe(return_df_most, striped=True, bordered=True), dbc.Table.from_dataframe(return_df_least, striped=True, bordered=True)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
